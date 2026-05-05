295a5918ce7ba242b0f8c9c3563cd138fb823c03
Noerly <noerlyyvonnemaeaya@gmail.com>
2026-05-05 22:24:43 +0800

    Dashboard

--- src/script.js
+++ src/script.js
@@ -55,3 +55,17 @@ function fadeOut(e) {
       window.location.href = link;
     }, 300); // match CSS duration
 }
+
+const toggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('-translate-x-full');
+    overlay.classList.toggle('hidden');
+});
+
+overlay.addEventListener('click', () => {
+    sidebar.classList.add('-translate-x-full');
+    overlay.classList.add('hidden');
+});
