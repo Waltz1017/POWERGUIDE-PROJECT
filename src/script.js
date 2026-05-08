1d335f407f9cadd91782f32e6ab434611db1378f
Noerly <noerlyyvonnemaeaya@gmail.com>
2026-05-08 17:03:57 +0800

    Outage Map

--- src/script.js
+++ src/script.js
@@ -135,3 +135,17 @@ function closeProfile() {
         profile.classList.add("pointer-events-none");
     }, 300);
 }
+
+const toggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('-translate-x-full');
+    overlay.classList.toggle('hidden');
+});
+
+overlay.addEventListener('click', () => {
+    sidebar.classList.add('-translate-x-full');
+    overlay.classList.add('hidden');
+});
